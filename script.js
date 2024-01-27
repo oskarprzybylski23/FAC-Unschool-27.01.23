ca032604b42e8e8170af9ab048559b7bb1c8ec2b
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-01-27 11:32:00 +0000

    API key submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let apiKey = '';
+
 document.getElementById('textarea-submit').onclick = function () {
   let text = document.getElementById('user-text').value;
-  document.getElementById('user-text-placeholder').innerText = text;
+  document.getElementById('user-text-placeholder').innerText = `user: ${text}`;
 };
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById('API-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Prevents the default form submission action
+
+    let apiKey = document.getElementById('input-api-key').value;
+    document.getElementById('input-api-key').value = '';
+
+    console.log(apiKey);
+    return apiKey;
+  });
+});
